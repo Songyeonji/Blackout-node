a9f79bd3313e8f417c07211a11d1116ab5774171
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-06-23 16:40:03 +0900

    메모장 추가

--- src/App copy.js
+++ src/App copy.js
@@ -1,37 +1,81 @@
-import { useState } from 'react';
-import { AppBar, Toolbar } from "@mui/material";
+import { useState, useQuery } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './index.css';
 import moment from 'moment';
+import { AppBar, Toolbar } from "@mui/material";
+import { experimental_extendTheme } from '@mui/material';
 
-function App() {
+
+export default function CalendarPage() {
   const curDate = new Date(); // 현재 날짜
-  const [value, onChange] = useState(curDate); // 클릭한 날짜 (초기값으로 현재 날짜 넣어줌)
-  const activeDate = moment(value).format('YYYY-MM-DD'); // 클릭한 날짜 (년-월-일))
+  
 
-  return (
-    
-    <div className='app'>
+const [value, onChange] = useState(curDate);
+const day = moment(value).format('YYYY-MM-DD');
+const curDateTime = moment(curDate).format('MM-DD');
+
+
+const mark = [
+  '2023-06-10',
+  '2023-06-21',
+  '2023-06-02',
+  '2023-06-14',
+  '2023-06-27',
+  '2023-06-27',
+];
+
+const addContent = ({ date, view }) => {
+  // 해당 날짜(하루)에 추가할 컨텐츠의 배열
+  const contents = [];
+
+  // date(각 날짜)가  리스트의 날짜와 일치하면 해당 컨텐츠(이모티콘) 추가
+  if (mark.find((day) => day === moment(date).format('YYYY-MM-DD'))) {
+    contents.push(
+      <>
+        {/* <div className="dot"></div> */}
+        
+        <div key={day} className="dot">
+        </div>
+        
+      </>
+    );
+  }
+  return <div >{contents}</div> // 각 날짜마다 해당 요소가 들어감
+};
+
+
+
+return (
+  <>
+   <AppBar position="fixed">
+        <Toolbar>
+          <div className="flex-1"></div>
+          <span className="font-bold">Black out</span>
+          <div className="flex-1"></div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+
       <h1 className="mr-auto">React Calendar with Range</h1>
-      <div className='calendar-container'>
-        <Calendar
-        onChange={onChange}
-          value={value}
-          locale="en"
-          formatDay={(locale, date) => moment(date).format('D')}
-         // tileContent={addContent}
-          showNeighboringMonth={false}
-          // onActiveStartDateChange={({ activeStartDate }) =>
-          //   getActiveMonth(activeStartDate)}
-        />
-      </div>
-        <p className='mr-auto'>
-          <span className='bold'>Default selected date:</span>{' '}
-          {value.toDateString()}
-        </p>
-    </div>
-  );
-}
+  <div className='calendar-container'>
+    <Calendar
+      onChange={onChange}
+      value={value}
+      locale="en" 
+      formatDay={(locale, date) => moment(date).format('D')} // '일' 표시 x
+      // tileDisabled={({ date, view }) =>
+      //   moment(date).format('MM-DD') < curDateTime
+	    //   }
+      tileContent={addContent}
+    />
+    <p className='mr-auto'>
+    <span className='bold'>Default selected date:</span>{' '}
+    {value.toDateString()}
+    </p>
+  </div>
+    
+</>
 
-export default App;
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,63 +1,52 @@
-import { useState, useQuery } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-import './index.css';
-import moment from 'moment';
-import { experimental_extendTheme } from '@mui/material';
+import React, { useState } from "react";
+import moment from "moment";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
+const DiaryCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [diaryEntries, setDiaryEntries] = useState({});
 
-export default function CalendarPage() {
-  const currDate = new Date(); // 현재 날짜
-  
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
-const [value, onChange] = useState(currDate);
-const day = moment(value).format('YYYY-MM-DD');
-const currDateTime = moment(currDate).format('MM-DD');
+  const handleDiaryEntry = (event) => {
+    const { value } = event.target;
+    setDiaryEntries((prevDiaryEntries) => ({
+      ...prevDiaryEntries,
+      [moment(selectedDate).format("YYYY-MM-DD")]: value,
+    }));
+  };
 
+  const tileContent = ({ date }) => {
+    const entry = diaryEntries[moment(date).format("YYYY-MM-DD")];
+    if (entry) {
+      return <div className="dot"></div>;
+    }
+    return null;
+  };
 
-const mark = [
-  '2023-06-10',
-  '2023-06-21',
-  '2023-06-02',
-  '2023-06-14',
-  '2023-06-27',
-  '2023-06-27',
-];
-
-const addContent = ({ date, view }) => {
-  // 해당 날짜(하루)에 추가할 컨텐츠의 배열
-  const contents = [];
-
-  // date(각 날짜)가  리스트의 날짜와 일치하면 해당 컨텐츠(이모티콘) 추가
-  if (mark.find((day) => day === moment(date).format('YYYY-MM-DD'))) {
-    contents.push(
-      <>
-        {/* <div className="dot"></div> */}
-        
-        <div key={day} className="dot">
+  return (
+    <div>
+      <h1>Diary Calendar</h1>
+      <Calendar
+        onChange={handleDateChange}
+        value={selectedDate}
+        tileContent={tileContent}
+      />
+      {selectedDate && (
+        <div>
+          <h2>{moment(selectedDate).format("YYYY-MM-DD")}</h2>
+          <textarea
+            value={diaryEntries[moment(selectedDate).format("YYYY-MM-DD")] || ""}
+            onChange={handleDiaryEntry}
+            rows={5}
+          />
         </div>
-        
-      </>
-    );
-  }
-  return <div >{contents}</div> // 각 날짜마다 해당 요소가 들어감
+      )}
+    </div>
+  );
 };
 
-
-
-return (
-  <div className='maincalendar'>
-    <Calendar
-      onChange={onChange}
-      value={value}
-      locale="en" 
-      formatDay={(locale, date) => moment(date).format('D')} // '일' 표시 x
-      tileDisabled={({ date, view }) =>
-        moment(date).format('MM-DD') < currDateTime
-	      }
-      tileContent={addContent}
-    />
-  </div>
-  )
-}
-
+export default DiaryCalendar;
