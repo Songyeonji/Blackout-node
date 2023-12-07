5f3946965f08eb8453fad96ab54867d21475a48e
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-12-07 14:06:25 +0900

    커뮤니티 탭바 상단고정

--- src/LearnMorePage.js
+++ src/LearnMorePage.js
@@ -1,55 +1,94 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, createTheme, ThemeProvider, Snackbar, Alert } from "@mui/material";
+import { AppBar, Toolbar, createTheme, ThemeProvider, Box, Tabs, Tab, Typography } from "@mui/material";
+import PropTypes from 'prop-types';
 
 const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#6f48eb", // 보라색
-      },
+  palette: {
+    primary: {
+      main: "#6f48eb", // 보라색
     },
-  });
+  },
+});
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const LearnMorePage = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <AppBar position="fixed">
-        <Toolbar>
-          <div className="flex-1"></div>
-          <Link
-            to="/drink-recommendation"
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            <span className="font-bold">오늘의 술 추천</span>
-          </Link>
-          <div className="flex-1"></div>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </div>
-    
-    <div>
-      <h2>카드뉴스</h2>
-      <ul>
-        <li>
-          <Link to="/board/food">맛집</Link>
-        </li>
-        <li>
-          <Link to="/board/drink">술</Link>
-        </li>
-        <li>
-          <Link to="/board/snack">안주</Link>
-        </li>
-      </ul>
-    </div>
+      <div>
+        <AppBar position="fixed">
+          <Toolbar>
+            <div className="flex-1"></div>
+            <Link
+              to="/drink-recommendation"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              <span className="font-bold">오늘의 술 추천</span>
+            </Link>
+            <div className="flex-1"></div>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        
+        {/* 여기서부터 탭 바 */}
+        <Box sx={{ width: '100%', backgroundColor: theme.palette.background.paper }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="맛집" {...a11yProps(0)} />
+            <Tab label="술" {...a11yProps(1)} />
+            <Tab label="안주" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+
+        <div>
+          <CustomTabPanel value={value} index={0}>
+            Item One
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            Item Three
+          </CustomTabPanel>
+        </div>
+      </div>
     </ThemeProvider>
   );
 };
