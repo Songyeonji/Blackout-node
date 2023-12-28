807fd19a2551ac1054ff8d59b335e7ea393bbb75
Songyeonji <104824428+Songyeonji@users.noreply.github.com>
2023-12-28 16:24:56 +0900

    로그인 백엔드 연결 성공

--- src/pages/member/Loginpage.jsx
+++ src/pages/member/Loginpage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useHistory} from 'react-router-dom';
+import axios from 'axios'; 
 import './Login.css';
 import { FaUser, FaLock } from "react-icons/fa";
 import { AppBar, Toolbar, createTheme, ThemeProvider } from "@mui/material";
@@ -13,11 +14,23 @@ const theme = createTheme({
 });
 
 const Loginpage = () => {
-    const [password, setPassword] = useState('');
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-      };
+  const history = useHistory();
+  const [loginId, setLoginId] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleLogin = async (event) => {
+      event.preventDefault();
+      try {
+          const response = await axios.post('http://localhost:8080/usr/member/doLogin', {
+              loginId,
+              loginPw: password
+          });
+          console.log('Logged in user:', response.data);
+          history.push('/'); // 로그인 성공 후 리다이렉트
+      } catch (error) {
+          console.error('Login failed:', error);
+      }
+  };
     return (
         <div className="member">
         <ThemeProvider theme={theme}>
@@ -45,25 +58,23 @@ const Loginpage = () => {
           </AppBar>
 
         <div className='wrapper'>
-            <form action="">
-                <h1>Login</h1>
-                <div className="input-box">
-                    <input type="text" placeholder='Login Id' required />
-                    <FaUser className='icon' />
-                </div>
-                <div className="input-box">
-                    <input type="password" placeholder='Password' value={password} onChange={handlePasswordChange} required />
-                    <FaLock className='icon' />
-                </div>
+        <form onSubmit={handleLogin}>
+                            <h1>Login</h1>
+                            <div className="input-box">
+                                <input type="text" placeholder='Login Id' value={loginId} onChange={(e) => setLoginId(e.target.value)} required />
+                                <FaUser className='icon' />
+                            </div>
+                            <div className="input-box">
+                                <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+                                <FaLock className='icon' />
+                            </div>
 
-                
+                            <button type="submit">Login</button>
 
-                <button type="submit">Login</button>
-
-                <div className="register-link">
-                    <p>Don't have an account? <Link to="/register">Register</Link></p>
-                </div>
-            </form>
+                            <div className="register-link">
+                                <p>Don't have an account? <Link to="/register">Register</Link></p>
+                            </div>
+                        </form>
         </div>
         </div>
         </ThemeProvider>
